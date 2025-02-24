187e011c31016b7b90e96c7de54964fdeb2243b9
Zac-Chen-2024 <zacchen2024@gmail.com>
2025-02-23 23:14:02 -0500

    Initial commit from new device

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # TnS-web
-Tap&amp;Say Web version
+Long press near the area you want to edit for one second to bring up the floating box. 
+
+Speak the changes you want to make, and you'll receive feedback from the Tap&Say large language model.
--- script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("voice-btn").addEventListener("click", function() {
-    let recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
-    recognition.lang = "en-US";
-    recognition.start();
-    recognition.onresult = function(event) {
-        let recognizedText = event.results[0][0].transcript;
-        console.log("语音输入:", recognizedText);
-    };
-});
